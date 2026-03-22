6d7463770b27796a18009c1ec725f217c1da6b17
Petr Abdulin <pabdulin@gmail.com>
2026-03-23 01:09:24 +0700

    build script and vscode configs

---
+++ build_debug.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set OUT_DIR=build\debug
+if not exist %OUT_DIR% mkdir %OUT_DIR%
+
+odin build src -out:%OUT_DIR%\game_debug.exe -debug
+IF %ERRORLEVEL% NEQ 0 exit /b 1
+
+xcopy /y /e /i assets %OUT_DIR%\assets > nul
+IF %ERRORLEVEL% NEQ 0 exit /b 1
+
+echo Debug build created in %OUT_DIR%
